bd840f12509f0bd2ab63bdb4341bab6457680b0d
MakiavelliPac <ridesburak@gmail.com>
2020-04-27 11:48:04 -0500

    Se creo la logica para el componente Glitch y se obtuvo una prueba satisfactoria en el uso de props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Glitch from './component/Glitch'
 
 function App() {
   return (
     <div >
-      Portfolio
+      <Glitch name='Santiago'/>
     </div>
   );
 }
---
+++ src/component/Glitch/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Glitch =(props) =>(
+    <div>
+        {props.name}
+    </div>
+)
+export default Glitch
