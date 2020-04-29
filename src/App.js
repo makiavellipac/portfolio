f941ce3d8e995d430f00f31fa3c30ac3c1708c0d
MakiavelliPac <ridesburak@gmail.com>
2020-04-29 16:57:13 -0500

    Se modifico el componente Typewriter para que se ejecute con array

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Glitch from './component/Glitch'
 import TypeWriter from './component/TypeWriter'
 
+
+
 function App() {
+  const perfil=[
+    "Full Stack Web Developer",
+    "MERN Stack",
+    "MEAN Stack",
+    "Ing. Ciencias De La Computación"
+  ]
   return (
     <>  
       <div className="glitch" data-text="Santiago AGM">
       <Glitch name='Santiago AGM'/>
       </div>
-      <TypeWriter perfil="Full Stack Web Developer"/>
+      <TypeWriter perfil={perfil}/>
     </>
   );
 }
--- src/component/TypeWriter/index.js
+++ src/component/TypeWriter/index.js
@@ -4,14 +4,17 @@ import './index.css'
 
 const TypeWriter =(props) =>{
  const [count,setCount]=useState(0);
- const [delay,setDelay]=useState(200);
+ const [delay,setDelay]=useState(150);
  const [meaning,setMeaning]=useState(true);
- const [size]=useState(props.perfil.length+5);
+ const [sizeArray]=useState(props.perfil.length);
+ const [size,setSize]=useState(props.perfil[0].length+5);
+ const [position,setPosition]=useState(0);
  
  const subs = (perfil,num)=>perfil.substring(0,num);
 
  useEffect(()=>{
      let interval=null;
+
      if(meaning){
          interval=setInterval(()=>{
              setCount(count+1);
@@ -27,19 +30,21 @@ const TypeWriter =(props) =>{
         },delay)
         if(count===0){
             setMeaning(!meaning);
-            setDelay(200);
+            setDelay(150);
+            sizeArray===position+1?setPosition(0):setPosition(position+1);
         }
      }
+     setSize(props.perfil[position].length+5);
      return()=>clearInterval(interval);
 
- },[count,delay,meaning,size])
+ },[count,delay,meaning,size,position,sizeArray,props.perfil])
 
 
 
 return(
     <div className="Typewriter">
         <span className="Tipewriter_wraper">
-            {subs(props.perfil,count)}
+            {subs(props.perfil[position],count)}
         </span>
         <span className="Tipewriter_cursor">|</span>
     </div>
