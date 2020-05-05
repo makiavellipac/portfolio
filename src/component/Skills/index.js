a1ff6d7146fcecea8dded56629061daddf8e9a1e
MakiavelliPac <ridesburak@gmail.com>
2020-05-05 00:57:30 -0500

    Se termino la pagina about con estilos y se agregaron las skills

--- src/component/Skills/index.js
+++ src/component/Skills/index.js
@@ -11,7 +11,16 @@ const Skills = ()=>{
             <div className="skills-container">
                 <Progress name="React"  value="90"/>
                 <Progress name="Angular"  value="40"/>
-                <Progress name="Vue"  value="10"/>
+                <Progress name="Vue"  value="20"/>
+                <Progress name="Node Js"  value="60"/>
+                <Progress name="Express"  value="90"/>
+                <Progress name="Mongo Db"  value="60"/>
+                <Progress name="My Sql"  value="50"/>
+                <Progress name="JavaScript"  value="95"/>
+                <Progress name="Java"  value="75"/>
+                <Progress name="Phyton"  value="35"/>
+                <Progress name="Php"  value="25"/>
+                <Progress name="Css"  value="50"/>
             </div>
         </div>
     )
