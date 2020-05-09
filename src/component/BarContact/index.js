6d708efdbd1e6641870da41f5600c68bfca24e2a
MakiavelliPac <ridesburak@gmail.com>
2020-05-09 17:50:13 -0500

    Se creo el enlace de github y linkedin

--- src/component/BarContact/index.js
+++ src/component/BarContact/index.js
@@ -13,8 +13,13 @@ const BarContact =()=>{
             <div className="container">
                 <ContactComponent icon={<IconPhone/>} title="Telefono" text="22 25 40 80 08" animation="fade-left"/>
                 <ContactComponent icon={<IconMail/>} title="Correo" text="ridesburak@gmail.com" animation="fade-up"/>
-                <ContactComponent icon={<IconLinkedin/>} title="Linkedin" text="santiago-gonzalez-mancilla" animation="fade-down"/>
-                <ContactComponent icon={<IconGithub/>} title="Github" text="makiavellipac" animation="fade-right"/>
+                <a href="https://www.linkedin.com/in/santiago-gonzalez-mancilla/" rel="noopener noreferrer" target="_blank">
+                    <ContactComponent icon={<IconLinkedin/>} title="Linkedin" text="santiago-gonzalez-mancilla" animation="fade-down" />
+                </a>
+                <a href="https://github.com/makiavellipac" rel="noopener noreferrer" target="_blank">
+                    <ContactComponent icon={<IconGithub/>} title="Github" text="makiavellipac" animation="fade-right"/>
+                </a>
+                
             </div>
         </div>
     )
